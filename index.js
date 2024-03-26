6bb91d01f3bdd086ae983c3177e1ac7e73b5c4b3
Nishikanta Sahoo <119565320+Nishi-kanta@users.noreply.github.com>
2024-03-26 15:53:33 +0530

    file

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const fs = require('fs');
 app.use(express());
 
 // Task 1: Basic Node.js Server
-// Create a simple Node.js server that listens on port 3000 and responds with "Hello, World!" for all incoming HTTP requests.You can use any library or framework of your choice
-app.get('/hello', async (req, res) => {
+app.get('/Hello', async (req, res) => {
     try {
         res.status(200).json({message:"Hello World"})
     } catch (err) {
@@ -16,10 +15,9 @@ app.get('/hello', async (req, res) => {
 })
 
 // Task 2: Data Manipulation
-// Write a function in Node.js that takes an array of integers as input and returns the sum of all the numbers.
-app.get('/sumofintegers', async (req, res) => {
+app.get('/sum', async (req, res) => {
     try {
-        const arr = [2, 12, 23, 45]
+        const arr = [5,58,7,56]
         const initialValue = 0;
         const totalSum = arr.reduce(
             (accumulator, currentValue) => accumulator + currentValue,
@@ -33,7 +31,6 @@ app.get('/sumofintegers', async (req, res) => {
 })
 
 // Task 3: Asynchronous File Handling
-// Create a Node.js script that reads a text file named "data.txt" and counts the number of words in it.The script should print the total word count to the console.
  function countWords(text) {
   return text.split(/\s+/).filter(word => word !== '').length;
  }
